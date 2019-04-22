b31e355ea4fc0f63bd7e710c9369a3ef72202aff
Sutinan Tadalimavad <lion.sutinan@hotmail.com>
2019-04-22 17:21:15 +0700

    validate.h added

---
+++ validate.h
@@ -0,0 +1,73 @@
+/*
+ * validate.h
+ * this program use to check validate of user input
+ * Created by Sutinan Tadalimavad (Lion) on 22/04/2019
+ * ID 61070503443
+ *
+*/
+
+/* This function use to check input from menu
+ * return 1 - if input is correct
+ * return 0 - if input is invalid
+*/
+int checkNumber(int choice)
+
+/* This function use to check flight code if it is valid code 
+ * return 1 - if flight code is correct
+ * return 0 - if flight is unvalid
+*/
+int checkCode(void* flight)
+
+/* This function use to check flight number if it is number or not
+ * return 1 - if flight number is in correct form
+ * return 0 - if flight number is invalid
+*/
+int checkNumberFlight(void* flight)
+
+/* This fucntion use to check flight code if it is correct
+ * return 1 - if code is valid
+ * return 0 - if code is invalid
+*/
+int checkFlightCode(void* flight)
+
+
+/* This function use to check direction from user
+ * direction must be N,NE,E,SE,S,SW,W,NW
+ * return 1 - if it is valid
+ * return 0 - if it is invalid
+*/
+int checkDirection(int direction)
+
+
+/* This functionuse to check if user give the correct height
+ * height must be between 3000 - 6000
+ * return 1 - if input is valid
+ * return 0 - if input is invalid
+*/
+int checkAltitude(int height)
+
+
+/* This function use to check if user give valid command for
+ * "order" command
+ * commands could be "landing","circle","takeoff"
+ * return 1 - if it is valid
+ * return 0 - if it is invalid
+*/
+int checkOrder(char command[])
+
+
+/* This function use to check command from user
+ * commands must be "direction","altitude","order"
+ * return 1 - if command is correct
+ * return 0 - if command is invalid
+*/
+int checkCommand(char input[])
+
+
+/* This function use to check command from user
+ * return 1 - if it is "status"
+ * return 2 - if it is "command"
+ * return 0 - if it is no match
+*/
+int checkAgru(char input[])
+	
