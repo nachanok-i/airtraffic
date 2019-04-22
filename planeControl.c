e54ab460b5cebf95c6cbf412f85cf9d24f51549f
Sahachok Prachaporn <lionsahachok@gmail.com>
2019-04-22 21:52:11 +0700

    plane control

--- planeControl.c
+++ planeControl.c
@@ -1,4 +1,4 @@
-/* planeControl.c
+/* planeControl->c
  *
  * control the plane position (moving) in the air and recieve command
  * from user and send it to control specific plane
@@ -10,38 +10,37 @@
 
 /* this function will move all the plane in continuously to the direction
  * that the plane is heading to */
-void movePlane()
+void movePlane(PLANE_T* airPlane)
 	{
-	PLANE_T airPlane;
-	switch (airPlane.heading)
+	switch (airPlane->heading)
 		{
 		case N:
-			airPlane.position.y -= 1;
+			airPlane->position->y -= 1;
 			break;
 		case NE:
-			airPlane.position.y -= 1;
-			airPlane.position.x += 1;
+			airPlane->position->y -= 1;
+			airPlane->position->x += 1;
 			break;
 		case E:
-			airPlane.position.x += 1;
+			airPlane->position->x += 1;
 			break;
 		case SE:
-			airPlane.position.x += 1;
-			airPlane.position.y += 1;
+			airPlane->position->x += 1;
+			airPlane->position->y += 1;
 			break;
 		case S:
-			airPlane.position.y += 1;
+			airPlane->position->y += 1;
 			break;
 		case SW:
-			airPlane.position.x -= 1;
-			airPlane.position.y += 1;
+			airPlane->position->x -= 1;
+			airPlane->position->y += 1;
 			break;
 		case W:
-			airPlane.position.x -= 1;
+			airPlane->position->x -= 1;
 			break;
 		case NW:
-			airPlane.position.x -= 1;
-			airPlane.position.y -= 1;
+			airPlane->position->x -= 1;
+			airPlane->position->y -= 1;
 			break;
 		/* normally this would not happen */
 		default:
