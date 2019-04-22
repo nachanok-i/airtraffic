e281ea26eba3600aa7385ca4716f6113267443c8
Sahachok Prachaporn <lionsahachok@gmail.com>
2019-04-22 19:19:40 +0700

    edit coment

--- menu.c
+++ menu.c
@@ -1,5 +1,5 @@
 /*This function is use to display menu and allow user to choose menu they want to do*
- * Created by: Sahachok Prachaporn ID:3436 (Lion)
+ * Created by Sahachok Prachaporn (Lion) ID 61070503436
  */
 #include<stdio.h>
 #include<stdlib.h>
