6af61c0f474c38bc02fdf4bd2e461c6cb9530e04
Sahachok Prachaporn <lionsahachok@gmail.com>
2019-05-20 17:13:10 +0700

    edit cmm

--- planeControl.c
+++ planeControl.c
@@ -161,7 +161,7 @@ void helpMenu()
 int main()
 	{
 	PLANE_T* plane = NULL;
-	int command = 0;
+	int command = 0; /* command variable*/
 	srand(time(NULL));
 	setup();
 	plane = generateFlight(100);
